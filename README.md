9aaf775600d14a523cce86a7d5fb1096981e98fa
Daniel Pérez <daniel.prz.c@gmail.com>
2021-05-27 08:59:19 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+<div align="center">
+  <img alt="Logo" src="" width="100" />
+</div>
+<h1 align="center">
+  Marvel Pages
+</h1>
+<p align="center">
+  First iteration of my personal website / portfolio. Built with <a href="https://reactjs.org/" target="_blank">React</a> and hosted with <a href="https://www.netlify.com/" target="_blank">Netlify</a>
+</p>
+<!-- <p align="center">
+  <a href="https://app.netlify.com/sites/danielperezcastejon/deploys" target="_blank">
+    <img src="https://api.netlify.com/api/v1/badges/986a3e71-6a62-45be-82e4-96327274546d/deploy-status" alt="Netlify Status" />
+  </a>
+</p> -->
+
+![demo](https://raw.githubusercontent.com/DanielPrzC/Personal-Website-V1/master/public/images/demo.png)
+
+## 🛠 Set Up
+
+1. Register at the <a href="https://developer.marvel.com" target="_bkank">Marvel´s Developers Site</a> To get your API keys and follow their instructions on how to use them.
+
+2. On the index.js file, replace the const:
+
+   ```render-babel
+   const APIPublicKey = //Your Public Key
+   const APITs = //Your TS
+   const APIHash = //Your Hash
+   ```
+following Marvel's instructions and you are good to test out this code.
+
+## 🎨 Color Reference
+
+| Color          | Hex                                                                |
+| -------------- | ------------------------------------------------------------------ |
+| Pure Black     | ![#000000](https://via.placeholder.com/10/000000?text=+) `#000000` |
+| Black          | ![#151515](https://via.placeholder.com/10/151515?text=+) `#151515` |
+| Dark Red       | ![#831518](https://via.placeholder.com/10/831518?text=+) `#831518` |
+| Red            | ![#e62429](https://via.placeholder.com/10/e62429?text=+) `#e62429` |
+| Gray           | ![#999999](https://via.placeholder.com/10/999999?text=+) `#999999` |
+| Light Gra      | ![#eaeaea](https://via.placeholder.com/10/eaeaea?text=+) `#eaeaea` |
+| White          | ![#ffffff](https://via.placeholder.com/10/ffffff?text=+) `#ffffff` |
---
+++ app.js
@@ -0,0 +1,109 @@
+const APIPublicKey = config.APIPublicKey;
+const APIts = config.APIts;
+const APIHash = config.APIHash;
+let APIOffset = 0;
+let totalCount;
+let searchQuery;
+
+const inputSearch = document.getElementById("input-search");
+const outputList = document.getElementById("output-list");
+const tabs = document.getElementsByClassName("tabs");
+
+window.addEventListener("load", () => {
+  fetchCharacters();
+});
+
+inputSearch.addEventListener("change", () => {
+  searchQuery = inputSearch.value;
+  fetchCharacters(searchQuery);
+});
+
+const next = async () => {
+  if (APIOffset >= 0 && APIOffset < Math.floor(totalCount / 20) * 20) {
+    APIOffset += 20;
+    fetchCharacters(searchQuery);
+  }
+};
+
+const previous = async () => {
+  if (APIOffset < Math.floor(totalCount / 20) * APIOffset || APIOffset > 0) {
+    APIOffset -= 20;
+    fetchCharacters(searchQuery);
+  }
+};
+
+const first = async () => {
+  if (APIOffset !== 0) {
+    APIOffset = 0;
+    fetchCharacters(searchQuery);
+  }
+};
+
+const last = async () => {
+  if (APIOffset !== Math.floor(totalCount / 20) * 20) {
+    APIOffset = Math.floor(totalCount / 20) * 20;
+    fetchCharacters(searchQuery);
+  }
+};
+
+const setData = (data) => {
+  outputList.innerHTML = "";
+
+  if (data.length !== 0) {
+    data.map((character) => {
+      const contentHTML = `<li class="character-card">
+      <a href=${character.urls[0].url} target="_blanck">
+        <div class="card-wrapper">
+          <div class="card-img">
+            <img src="${character.thumbnail.path}.${
+        character.thumbnail.extension
+      }" alt="${character.name}" class="img-thumbnail" />
+          </div>
+          <div class="back-img">
+            <p>${
+              character.description !== "" && character.description.length > 140
+                ? `${character.description.substring(0, 137)}...`
+                : "No description available for this character."
+            }</p>
+            <p class="cta">CLICK FOR MORE INFO!</p>
+          </div>
+          <div class="card-title">
+            <h3 class="title">${character.name}</h3>
+          </div>
+        </div>
+      </a>
+    </li>`;
+
+      let outputString = document.createElement("div");
+      outputString.innerHTML = contentHTML;
+      outputList.appendChild(outputString);
+    });
+  } else {
+    const notFoundHTML = `<h3 class="not-found">SORRY, NOTHING TO SEE HERE</h3>`;
+
+    let outputString = document.createElement("div");
+    outputString.innerHTML = notFoundHTML;
+    outputList.appendChild(outputString);
+  }
+};
+
+const fetchCharacters = async (query) => {
+  let res;
+
+  if (query) {
+    res = await fetch(
+      `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${query}&limit=20&offset=${APIOffset}&ts=${APIts}&apikey=${APIPublicKey}&hash=${APIHash}`
+    );
+  } else {
+    res = await fetch(
+      `https://gateway.marvel.com:443/v1/public/characters?limit=20&offset=${APIOffset}&ts=${APIts}&apikey=${APIPublicKey}&hash=${APIHash}`
+    );
+  }
+
+  let results = await res.json();
+  totalCount = results.data.total;
+
+  let characters = results.data.results;
+  totalCount = results.data.total;
+  setData(characters);
+};
